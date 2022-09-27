8a7f092f8990f1043d358b4ae205041142c3ada2
Rachel Isaac <rachelisaac995@gmail.com>
2022-09-27 16:00:19 +0400

    setting up components and API

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = "https://restcountries.com/v3.1/all";
+
+  const [countries, setCountries] = useState([]);
+
+  const getCountries = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <Navbar />
+      </header>
+      {countries.map((country) => {
+        return <p>{country.name.common}</p>;
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <h1>Where in the world?</h1>
+        <div className="dark-mode-btn">
+          <img src="" alt="moon-icon" />
+          <p>Dark Mode</p>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
